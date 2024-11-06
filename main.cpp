689c4091eda873373603407737b9a65ece704b92
zuzuz <duda.zuzanna7@gmail.com>
2024-11-06 21:37:51 +0100

    reading ini file method implemented

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
-int main()
-{
+#define INIT_FILE "DataInit.txt"
 
+std::string method;
+std::string outputFileName;
+std::string outputFileExtention = ".csv";
+
+std::vector<std::string> testFileNames;
+std::vector<int> numberOfTestsList;
+
+void readIniFile(const std::string &iniFileName) {
+    std::ifstream iniFile(iniFileName);
+
+    if (iniFile.is_open()) {
+        std::string line;
+
+        getline(iniFile, line);
+        std::stringstream sstream(line);
+        sstream >> method;
+
+        while (getline(iniFile, line)) {
+            std::string testFileName;
+            std::stringstream sstream2(line);
+            sstream2 >> testFileName;
+
+            if(testFileName.find(outputFileExtention) != std::string::npos) {
+                outputFileName = testFileName;
+                break;
+            }
+
+            int numberOfTests;
+            sstream2 >> numberOfTests;
+
+            testFileNames.push_back(testFileName);
+            numberOfTestsList.push_back(numberOfTests);
+
+        }
+    } else {
+        std::cout << "Errror while trying to open ini file.";
+    }
+}
+
+int main() {
     return 0;
 }
